6b8d706868cceb68a381da80a532df0cc977564b
akandel <akandel@quantium.com.au>
2018-11-29 16:34:22 +1100

    optimize image

---
+++ public/path----8f6fee8f8b30d2d4236a.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{108:function(t,a){t.exports={data:{site:{siteMetadata:{title:"incessantmeraki"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----8f6fee8f8b30d2d4236a.js.map
